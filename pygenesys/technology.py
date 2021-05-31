5b5bb7605976c92fcf62a78fda04d70b7974e3db
samgdotson <sgd2@illinois.edu>
2021-05-31 13:44:05 -0500

    adds pygenesys module folder and tests

---
+++ pygenesys/technology.py
@@ -0,0 +1,8 @@
+def placeholder():
+    """
+    This function does nothing except
+    prints the Zen of Python.
+    """
+    import this
+
+    return
---
+++ pygenesys/tests/test_pygenesys.py
@@ -0,0 +1,13 @@
+from pygenesys import technology
+
+def test_technology_placeholder():
+    """
+    Tests the placeholder function in
+    pygenesys.technology
+    """
+
+    ret_value = technology.placeholder()
+
+    assert(ret_value == None)
+
+    return
